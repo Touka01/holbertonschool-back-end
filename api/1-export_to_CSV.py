b051c98bd693ddef89db7e718cc6251b6ed9df02
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-09-12 19:53:43 +0000

    csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 '''
-This module defines the REST API
+This module defines a script that interacts with a REST API to fetch and export
+employee task data in CSV format.
 '''
+
 import requests
 import csv
 import sys
@@ -10,18 +12,47 @@ BASE_URL = 'https://jsonplaceholder.typicode.com'
 
 
 def get_user_info(id):
+    '''
+    Fetch user information by user ID.
+
+    Args:
+        id (int): The ID of the user.
+
+    Returns:
+        dict: A dictionary containing user information.
+    '''
     response = requests.get(f'{BASE_URL}/users/{id}')
     response.raise_for_status()
     return response.json()
 
 
 def get_user_todos(id):
+    '''
+    Fetch user's TODO tasks by user ID.
+
+    Args:
+        id (int): The ID of the user.
+
+    Returns:
+        list: A list of TODO tasks for the user.
+    '''
     response = requests.get(f'{BASE_URL}/todos', params={'userId': id})
     response.raise_for_status()
     return response.json()
 
 
 def export_user_tasks_to_csv(user_id, username, tasks):
+    '''
+    Export user's tasks to a CSV file.
+
+    Args:
+        user_id (int): The ID of the user.
+        username (str): The username of the user.
+        tasks (list): A list of tasks for the user.
+
+    Returns:
+        None
+    '''
     file_name = f'{user_id}.csv'
 
     with open(file_name, 'w', newline='') as csv_file:
@@ -49,4 +80,3 @@ if __name__ == "__main__":
         print("Error: Employee ID must be an integer.")
     except requests.RequestException as e:
         print(f"An error occurred: {e}")
-chmod
